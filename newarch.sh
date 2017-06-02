8e219a92584afc213729a82aa6a8a3d80cc45a6d
eier7 <eivind.ervik@gmail.com>
2017-06-02 07:45:32 +0000

    newarch

--- newarch.sh
+++ newarch.sh
@@ -3,21 +3,21 @@
 sudo pacman -Syy
 sudo pacman -S base-devel fakeroot wget vim --noconfirm
 
-wget https://aur.archlinux.org/packages/pa/package-query-git/package-query-git.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query-git.tar.gz
 tar -xvf package-query-git.tar.gz
 cd package-query-git
 makepkg -s -f
 sudo pacman -U --noconfirm package-query-git-*-*.pkg.tar.xz
 cd ..
 
-wget https://aur.archlinux.org/packages/ya/yaourt-git/yaourt-git.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt-git.tar.gz
 tar -xvf yaourt-git.tar.gz
 cd yaourt-git
 makepkg -s -f
 sudo pacman -U --noconfirm yaourt-git-*-*.pkg.tar.xz
 cd ..
 
-sudo yaourt -Syy --noconfirm
-sudo yaourt -S tmux python-pyserial python-raspberry-gpio --noconfirm
+yaourt -Syy --noconfirm
+yaourt -S tmux python-pyserial python-raspberry-gpio --noconfirm
 sudo cp vimrc /etc/vim/vimrc
 
